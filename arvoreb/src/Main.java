929482262f969a439bea7e65b3dc7d0f8e1833fc
Ferrari <98551353+WlFerrari@users.noreply.github.com>
2025-05-20 20:07:07 -0300

    Contar nós

--- arvoreb/src/Main.java
+++ arvoreb/src/Main.java
@@ -1,19 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        class No {
-            String item;
-            No esquerda, direita;
 
-            public No (String item, String raiz){
-                this.item = item;
-                esquerda = direita = null;
-            }
+    }
+
+    static class No {
+        String item;
+        No esquerda, direita;
+        int node;
+
+        public No(String item) {
+            this.item = item;
+            esquerda = direita = null;
         }
+    }
 
-        class Arvore {
+    static class Arvore {
+        No raiz;
+    }
 
-            No raiz;
-
+    public static int contarNos(No node) {
+        if (node == null) {
+            return 0;
         }
+        return 1 + contarNos(node.esquerda) + contarNos(node.direita);
     }
 }
