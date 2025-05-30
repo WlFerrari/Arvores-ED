1d6ec9eecc6da6e222658493db78073851c63c84
Ferrari <98551353+WlFerrari@users.noreply.github.com>
2025-05-29 21:38:52 -0300

    Adicionando contar nos com fila

---
+++ arvoreb/src/ContadorNosFila/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Arvore arvore = new Arvore();
+
+        arvore.inserir(10);
+        arvore.inserir(5);
+        arvore.inserir(15);
+        arvore.inserir(3);
+        arvore.inserir(7);
+
+        int totalNos = arvore.contarNosComFila();
+        System.out.println("Total de nós na árvore: " + totalNos);
+    }
+}
